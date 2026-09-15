6090b086d95dbe57f531b507e2ecaeccd4ce8dd1
Harsh <h4r5h.c0d3s@gmail.com>
2026-09-15 07:17:38 +0530

    feat: add helper function to get first error message

--- src/utils/cleanup-helpers.js
+++ src/utils/cleanup-helpers.js
@@ -1,6 +1,22 @@
+const getFirstErrorMessage = (errors) => {
+  for (const value of Object.values(errors)) {
+    if (value?.message) {
+      return value.message;
+    }
+
+    if (typeof value === "object" && value !== null) {
+      const message = getFirstErrorMessage(value);
+
+      if (message) return message;
+    }
+  }
+
+  return undefined;
+};
+
 export const getOnlyErrorMessage = (error) => {
   if (error) {
-    let errorMessage = error.details[0].message;
+    let errorMessage = getFirstErrorMessage(error.details)
 
     // it will replace the double quotes from error like '"name" is required' - result -> 'name is required'
     errorMessage = errorMessage.replaceAll('"', "");
